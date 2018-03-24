6f28c2e12c4f3691ca4a1c4a297fe56472e1f8d4
Christen Ward <christen@christenward.com>
2018-03-24 14:03:55 -0500

    Check platform before using native plugins

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,11 +13,13 @@ export class MyApp {
 
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private _translate: TranslateService) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
-    });
+    if (platform.is('core')) {
+      platform.ready().then(() => {
+        // Okay, so the platform is ready and our plugins are available.
+        // Here you can do any higher level native things you might need.
+        statusBar.styleDefault();
+        splashScreen.hide();
+      });
+    }
   }
 }
